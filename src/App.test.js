d678edb3262e41c12b878fec48a6778b1ba1111e
Ryan Vice <rvice@barefootnetworks.com>
2018-03-11 22:13:46 -0500

    Added removed async test code to match the blog post pacing.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { mount } from 'enzyme';
-import sinon from 'sinon';
 import Adapter from 'enzyme-adapter-react-16';
-import App, { http } from './App';
+import App from './App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -17,53 +16,12 @@ describe('Given we load our app ', () => {
   it('Then it shows a loading indicator', () => {
     const app = mount(<App/>);
 
-    expect(app).toBeTruthy();
-  });
+    expect(app.getElements().length).toBeTruthy();
 
-  describe('When we have posts on the server ', () => {
-    it('Then we get posts written to the screen', done => {
-      http.get = sinon.stub();
+    const loadingIndicator = app.find('h3');
 
-      http.get
-        .withArgs('/posts')
-        .returns(Promise.resolve(getDummyPosts()));
+    expect(loadingIndicator.getElements().length).toBeTruthy();
 
-      const app = mount(<App/>);
-
-      expect(app).toBeTruthy();
-
-      setTimeout(() => {
-        app.update();
-
-        const posts = app.find('li');
-
-        expect(posts.getElements().length).toBe(3);
-
-        done();
-      });
-    });
+    expect(loadingIndicator.getElements()[0].props.children).toBe('Loading...');
   });
 });
-
-function getDummyPosts() {
-  return [
-    {
-      "userId": 1,
-      "id": 1,
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-      "userId": 1,
-      "id": 2,
-      "title": "qui est esse",
-      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-      "userId": 1,
-      "id": 3,
-      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-    }
-  ];
-}
